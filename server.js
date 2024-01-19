cf331c7cb50844db6515eb224fecbb24becac79c
Abhishek borana <76511435+abhishekborana@users.noreply.github.com>
2024-01-19 12:04:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 const PORT =8080;
-fs.readFile('.index.html',function(error,html){
-  if(error) throw error;
+fs.readFile('./index.html',function(error,html){
   http.createServer(function(request,respone){
     response.writeHeader(200,{"Content-Type":"text/html"});
     response.write(html);
